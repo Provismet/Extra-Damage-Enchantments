c637aa16f046cc4636ca961548b3041d2d8a036d
Provismet <rhys.williams343@gmail.com>
2022-12-07 04:22:38 +0000

    Add additional entity groups.

---
+++ src/main/java/com/provismet/extradamageenchants/group/ExtraGroups.java
@@ -0,0 +1,12 @@
+package com.provismet.extradamageenchants.group;
+
+import net.minecraft.entity.EntityGroup;
+
+public class ExtraGroups {
+    public static final EntityGroup NULL = new EntityGroup(); // This group is never assigned and only acts as a default case to guard against errors.
+    public static final EntityGroup HUMANOID = new EntityGroup();
+    public static final EntityGroup OUTERWORDLY = new EntityGroup();
+    public static final EntityGroup ARCANE = new EntityGroup();
+    public static final EntityGroup FUNGIFLORAL = new EntityGroup();
+    public static final EntityGroup BEASTLY = new EntityGroup();
+}
