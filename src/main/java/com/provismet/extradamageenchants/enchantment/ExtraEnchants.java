18b3fce44a9524d5f437019e07b45a402c50936d
Provismet <rhys.williams343@gmail.com>
2022-12-15 23:10:08 +0000

    Correctly register enchantments.

--- src/main/java/com/provismet/extradamageenchants/enchantment/ExtraEnchants.java
+++ src/main/java/com/provismet/extradamageenchants/enchantment/ExtraEnchants.java
@@ -1,15 +1,24 @@
 package com.provismet.extradamageenchants.enchantment;
 
 import com.provismet.extradamageenchants.ExtraDEMain;
+import com.provismet.extradamageenchants.group.ExtraGroup;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.util.registry.Registry;
 
 public class ExtraEnchants {
-    public static final Enchantment REBEL = Registry.register(Registry.ENCHANTMENT, ExtraDEMain.identifier("rebel"), new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, 0, EquipmentSlot.MAINHAND));
-    public static final Enchantment VOIDRENDER = Registry.register(Registry.ENCHANTMENT, ExtraDEMain.identifier("voidrender"), new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, 1, EquipmentSlot.MAINHAND));
-    public static final Enchantment UNRAVEL = Registry.register(Registry.ENCHANTMENT, ExtraDEMain.identifier("unravel"), new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, 2, EquipmentSlot.MAINHAND));
-    public static final Enchantment HERBICIDE = Registry.register(Registry.ENCHANTMENT, ExtraDEMain.identifier("herbicide"), new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, 3, EquipmentSlot.MAINHAND));
-    public static final Enchantment BUTCHER = Registry.register(Registry.ENCHANTMENT, ExtraDEMain.identifier("butcher"), new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, 4, EquipmentSlot.MAINHAND));
+    public static final Enchantment REBEL = new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, ExtraGroup.HUMANOID, EquipmentSlot.MAINHAND);
+    public static final Enchantment VOIDRENDER = new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, ExtraGroup.OUTERWORDLY, EquipmentSlot.MAINHAND);
+    public static final Enchantment UNRAVEL = new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, ExtraGroup.ARCANE, EquipmentSlot.MAINHAND);
+    public static final Enchantment HERBICIDE = new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, ExtraGroup.FUNGIFLORAL, EquipmentSlot.MAINHAND);
+    public static final Enchantment BUTCHER = new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, ExtraGroup.BEASTLY, EquipmentSlot.MAINHAND);
+
+    public static void register () {
+        Registry.register(Registry.ENCHANTMENT, ExtraDEMain.identifier("rebel"), REBEL);
+        Registry.register(Registry.ENCHANTMENT, ExtraDEMain.identifier("voidrender"), VOIDRENDER);
+        Registry.register(Registry.ENCHANTMENT, ExtraDEMain.identifier("unravel"), UNRAVEL);
+        Registry.register(Registry.ENCHANTMENT, ExtraDEMain.identifier("herbicide"), HERBICIDE);
+        Registry.register(Registry.ENCHANTMENT, ExtraDEMain.identifier("butcher"), BUTCHER);
+    }
 }
