aee4ba82adf668d3f1c30ad5a4d83ce3fcb6620f
Provismet <rhys.williams343@gmail.com>
2022-12-15 23:11:16 +0000

    Improved enchantments.

--- src/main/java/com/provismet/extradamageenchants/enchantment/ExtraDamageEnchantment.java
+++ src/main/java/com/provismet/extradamageenchants/enchantment/ExtraDamageEnchantment.java
@@ -1,6 +1,6 @@
 package com.provismet.extradamageenchants.enchantment;
 
-import com.provismet.extradamageenchants.group.ExtraGroups;
+import com.provismet.extradamageenchants.group.ExtraGroup;
 
 import net.minecraft.enchantment.DamageEnchantment;
 import net.minecraft.enchantment.Enchantment;
@@ -8,23 +8,18 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityGroup;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 
 public class ExtraDamageEnchantment extends DamageEnchantment {
-    private static final EntityGroup[] groups = new EntityGroup[]{ExtraGroups.HUMANOID, ExtraGroups.OUTERWORDLY, ExtraGroups.ARCANE, ExtraGroups.FUNGIFLORAL, ExtraGroups.BEASTLY};
     private static final int BASE_POWER = 5;
     private static final int POWER_PER_LEVEL = 8;
     private static final int MIN_MAX_POWER_DIFFERENCE = 20;
     private final EntityGroup preferredGroup;
 
-    public ExtraDamageEnchantment (Enchantment.Rarity weight, int typeIndex, EquipmentSlot ... slots) {
-        super(weight, typeIndex, slots);
-
-        if (typeIndex < 0 || typeIndex >= ExtraDamageEnchantment.groups.length) {
-            this.preferredGroup = ExtraGroups.NULL; // This group is never assigned.
-        }
-        else {
-            this.preferredGroup = groups[typeIndex];
-        }
+    public ExtraDamageEnchantment (Enchantment.Rarity weight, EntityGroup preferredGroup, EquipmentSlot ... slots) {
+        super(weight, -1, slots);
+        this.preferredGroup = preferredGroup;
     }
 
     @Override
@@ -39,7 +34,7 @@ public class ExtraDamageEnchantment extends DamageEnchantment {
 
     @Override
     public float getAttackDamage (int level, EntityGroup entityGroup) {
-        if (entityGroup == this.preferredGroup) {
+        if (entityGroup == this.preferredGroup || (this.preferredGroup == ExtraGroup.HUMANOID && entityGroup == EntityGroup.ILLAGER)) {
             return (float)level * 2.5f;
         }
         return 0.0f;
@@ -47,6 +42,17 @@ public class ExtraDamageEnchantment extends DamageEnchantment {
 
     @Override
     public void onTargetDamaged (LivingEntity user, Entity target, int level) {
-        // TODO: Ideas for this? Otherwise keep empty to avoid interacting with the super version's logic.
+        if (target instanceof LivingEntity) {
+            LivingEntity livingEntity = (LivingEntity)target;
+            if (livingEntity.getGroup() == this.preferredGroup) {
+                if (this.preferredGroup == ExtraGroup.OUTERWORDLY || this.preferredGroup == ExtraGroup.ARCANE) {
+                    livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 60, 1));
+                }
+                else if (this.preferredGroup == ExtraGroup.BEASTLY) {
+                    int i = 20 + user.getRandom().nextInt(10 * level);
+                    livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, i, 2));
+                }
+            }
+        }
     }
 }
