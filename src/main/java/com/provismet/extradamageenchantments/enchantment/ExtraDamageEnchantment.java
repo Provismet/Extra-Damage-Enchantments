90af6aac2a7150009d319355f522269b8404e27a
Provismet <rhys.williams343@gmail.com>
2023-01-31 23:33:39 +0000

    Code refactor and slight adjustments.
    ExtraDamageEnchantment is no longer dependent on any specific entity type, objects of the class may provide information to build a status effect.
    This change is non-existent for Butcher, but Unravel/Outerworldly now have a randomly increased glow duration.

--- src/main/java/com/provismet/extradamageenchantments/enchantment/ExtraDamageEnchantment.java
+++ src/main/java/com/provismet/extradamageenchantments/enchantment/ExtraDamageEnchantment.java
@@ -1,25 +1,42 @@
 package com.provismet.extradamageenchantments.enchantment;
 
-import com.provismet.extradamageenchantments.group.ExtraGroup;
-
 import net.minecraft.enchantment.DamageEnchantment;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityGroup;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 
 public class ExtraDamageEnchantment extends DamageEnchantment {
     private static final int BASE_POWER = 5;
     private static final int POWER_PER_LEVEL = 8;
     private static final int MIN_MAX_POWER_DIFFERENCE = 20;
-    private final EntityGroup preferredGroup;
+    
+    private final EntityGroup[] preferredGroups;
+    private final StatusEffect status;
+    private final int effectDuration;
+    private final int effectAmplifier;
+
+    public ExtraDamageEnchantment (Enchantment.Rarity weight, EntityGroup[] preferredGroups, StatusEffect effect, int effectAmplifier, int effectDuration, EquipmentSlot ... slots) {
+        super(weight, -1, slots);
+        this.preferredGroups = preferredGroups;
+        this.status = effect;
+        this.effectAmplifier = effectAmplifier;
+        this.effectDuration = effectDuration;
+    }
+
+    public ExtraDamageEnchantment (Enchantment.Rarity weight, EntityGroup[] preferredGroups, EquipmentSlot ... slots) {
+        this(weight, preferredGroups, null, 0, 0, slots);
+    }
 
     public ExtraDamageEnchantment (Enchantment.Rarity weight, EntityGroup preferredGroup, EquipmentSlot ... slots) {
-        super(weight, -1, slots);
-        this.preferredGroup = preferredGroup;
+        this(weight, new EntityGroup[] {preferredGroup}, slots);
+    }
+
+    public ExtraDamageEnchantment (Enchantment.Rarity weight, EntityGroup preferredGroup, StatusEffect effect, int effectAmplifier, int effectDuration, EquipmentSlot ... slots) {
+        this(weight, new EntityGroup[] {preferredGroup}, effect, effectAmplifier, effectDuration, slots);
     }
 
     @Override
@@ -34,25 +51,26 @@ public class ExtraDamageEnchantment extends DamageEnchantment {
 
     @Override
     public float getAttackDamage (int level, EntityGroup entityGroup) {
-        if (entityGroup == this.preferredGroup || (this.preferredGroup == ExtraGroup.HUMANOID && entityGroup == EntityGroup.ILLAGER)) {
-            return (float)level * 2.5f;
+        for (EntityGroup preferred : this.preferredGroups) {
+            if (preferred == entityGroup) return (float)level * 2.5f;
         }
         return 0.0f;
     }
 
     @Override
     public void onTargetDamaged (LivingEntity user, Entity target, int level) {
-        if (target instanceof LivingEntity) {
-            LivingEntity livingEntity = (LivingEntity)target;
-            if (livingEntity.getGroup() == this.preferredGroup) {
-                if (this.preferredGroup == ExtraGroup.OUTERWORDLY || this.preferredGroup == ExtraGroup.ARCANE) {
-                    livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 60, 1));
-                }
-                else if (this.preferredGroup == ExtraGroup.BEASTLY) {
-                    int i = 20 + user.getRandom().nextInt(10 * level);
-                    livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, i, 2));
-                }
+        if (target instanceof LivingEntity livingEntity) {
+            if (isPreferred(livingEntity.getGroup()) && this.status != null) {
+                int randomModifier = user.getRandom().nextInt(10 * level);
+                livingEntity.addStatusEffect(new StatusEffectInstance(this.status, this.effectDuration + randomModifier, this.effectAmplifier), user);
             }
         }
+    }
+
+    protected boolean isPreferred (EntityGroup group) {
+        for (EntityGroup preferred : this.preferredGroups) {
+            if (preferred == group) return true;
+        }
+        return false;
     }
 }
--- src/main/java/com/provismet/extradamageenchantments/enchantment/ExtraEnchants.java
+++ src/main/java/com/provismet/extradamageenchantments/enchantment/ExtraEnchants.java
@@ -4,21 +4,24 @@ import com.provismet.extradamageenchantments.ExtraDEMain;
 import com.provismet.extradamageenchantments.group.ExtraGroup;
 
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.EntityGroup;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 
 public class ExtraEnchants {
-    public static final Enchantment REBEL = new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, ExtraGroup.HUMANOID, EquipmentSlot.MAINHAND);
-    public static final Enchantment VOIDRENDER = new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, ExtraGroup.OUTERWORDLY, EquipmentSlot.MAINHAND);
-    public static final Enchantment UNRAVEL = new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, ExtraGroup.ARCANE, EquipmentSlot.MAINHAND);
+    public static final Enchantment REBEL = new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, new EntityGroup[] {ExtraGroup.HUMANOID, EntityGroup.ILLAGER}, EquipmentSlot.MAINHAND);
+    public static final Enchantment VOIDRENDER = new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, ExtraGroup.OUTERWORDLY, StatusEffects.GLOWING, 0, 60, EquipmentSlot.MAINHAND);
+    public static final Enchantment UNRAVEL = new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, ExtraGroup.ARCANE, StatusEffects.GLOWING, 0, 60, EquipmentSlot.MAINHAND);
     public static final Enchantment HERBICIDE = new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, ExtraGroup.FUNGIFLORAL, EquipmentSlot.MAINHAND);
-    public static final Enchantment BUTCHER = new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, ExtraGroup.BEASTLY, EquipmentSlot.MAINHAND);
+    public static final Enchantment BUTCHER = new ExtraDamageEnchantment(Enchantment.Rarity.UNCOMMON, ExtraGroup.BEASTLY, StatusEffects.SLOWNESS, 2, 20, EquipmentSlot.MAINHAND);
 
     public static void register () {
-        Registry.register(Registry.ENCHANTMENT, ExtraDEMain.identifier("rebel"), REBEL);
-        Registry.register(Registry.ENCHANTMENT, ExtraDEMain.identifier("voidrender"), VOIDRENDER);
-        Registry.register(Registry.ENCHANTMENT, ExtraDEMain.identifier("unravel"), UNRAVEL);
-        Registry.register(Registry.ENCHANTMENT, ExtraDEMain.identifier("herbicide"), HERBICIDE);
-        Registry.register(Registry.ENCHANTMENT, ExtraDEMain.identifier("butcher"), BUTCHER);
+        Registry.register(Registries.ENCHANTMENT, ExtraDEMain.identifier("rebel"), REBEL);
+        Registry.register(Registries.ENCHANTMENT, ExtraDEMain.identifier("voidrender"), VOIDRENDER);
+        Registry.register(Registries.ENCHANTMENT, ExtraDEMain.identifier("unravel"), UNRAVEL);
+        Registry.register(Registries.ENCHANTMENT, ExtraDEMain.identifier("herbicide"), HERBICIDE);
+        Registry.register(Registries.ENCHANTMENT, ExtraDEMain.identifier("butcher"), BUTCHER);
     }
 }
